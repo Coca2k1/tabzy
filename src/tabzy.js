99fd59182a9ef946dfe69f667df5642c2aa7899d
boynguyen <boynguyen709@gmail.com>
2025-07-12 01:57:44 +0700

    Building Tabzy function contructor and logic for Tabzy

--- src/tabzy.js
+++ src/tabzy.js
@@ -1,24 +1,67 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const tabs = $$(".tab-btn");
-const contents = $$(".content-desc");
+function Tabzy(selector) {
+    this.container = document.querySelector(selector);
 
-tabs.forEach((btn) => {
-    btn.onclick = function (e) {
-        tabs.forEach((btn) => btn.classList.remove("active"));
+    if (!this.container) {
+        console.error(`Tabzy: No container found for selector: '${selector}'`);
+        return;
+    }
+    this.tabs = Array.from(this.container.querySelectorAll("li a"));
 
-        btn.classList.add("active");
+    if (!this.tabs.length) {
+        console.error("Tabzy: No tasks found inside in the container!");
+        return;
+    }
 
-        const title = $(btn.dataset.id);
-        const line = $(".line");
+    this.panels = this.tabs
+        .map((tab) => {
+            const panel = document.querySelector(tab.getAttribute("href"));
 
-        // content
-        contents.forEach((content) => content.classList.remove("active"));
-        title.classList.add("active");
+            if (!panel) {
+                console.error(
+                    `No Panel found for selector '${tab.getAttribute("href")}'`
+                );
+                return;
+            }
+            return panel;
+        })
+        .filter(Boolean);
 
-        // line
-        line.style.width = e.target.offsetWidth + "px";
-        line.style.left = e.target.offsetLeft + "px";
-    };
-});
+    if (this.panels.length !== this.tabs.length) return;
+
+    this._init();
+}
+
+Tabzy.prototype._init = function () {
+    const tabActive = this.tabs[0];
+    tabActive.closest("li").classList.add("active");
+
+    this.panels.forEach((panel) => (panel.hidden = true));
+
+    const panelActive = this.panels[0];
+    panelActive.hidden = false;
+
+    // Event
+    this.tabs.forEach(
+        (tab) => (tab.onclick = (event) => this._handleTabClick(event, tab))
+    );
+};
+
+Tabzy.prototype._handleTabClick = function (event, tab) {
+    event.preventDefault();
+
+    this.tabs.forEach((tab) => tab.closest("li").classList.remove("active"));
+    tab.closest("li").classList.add("active");
+
+    this.panels.forEach((panel) => {
+        panel.hidden = true;
+        panel.classList.remove("active");
+    });
+
+    // panel
+    const panelActive = document.querySelector(tab.getAttribute("href"));
+    panelActive.hidden = false;
+    panelActive.classList.add("active");
+};
